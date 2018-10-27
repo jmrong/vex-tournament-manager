1d07f93c6413ee9b57f9d1d4a5e8d0b86ad4cde2
bungularbike <rongjackmiles@gmail.com>
2018-10-27 21:08:58 +0800

    Login

--- index.js
+++ index.js
@@ -10,3 +10,71 @@ var config = {
 };
 firebase.initializeApp(config);
 
+firebase.auth().onAuthStateChanged(function(account) {
+
+	if (account) {
+
+		window.open("teams.html", "_self");
+
+	}
+
+});
+
+document.getElementsByTagName("form")[0].onsubmit = function(event) {
+
+	event.preventDefault();
+	if ($("input[name='team']:checked").val() == null) {
+
+		$(".custom-control-input, custom-control-label").addClass("is-invalid");
+		if ($("#password").val().length < 6) {
+
+			$("#password").addClass("is-invalid");
+
+		}
+
+	} else if ($("#password").val().length < 6) {
+
+		$("#password").addClass("is-invalid");
+
+	} else {
+
+		$("button").attr("disabled", "true");
+		firebase.auth().signInWithEmailAndPassword($("input[name='team']:checked").val() + "@hkis.edu.hk", $("#password").val()).then(function() {
+
+			firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+			window.open("teams.html", "_self");
+
+		}).catch(function(error) {
+
+			$("button").removeAttr("disabled");
+			if (error.code == "auth/wrong-password") {
+
+				$("#password").addClass("is-invalid");
+
+			} else {
+
+				alert("An error occurred: " + error.message);
+
+			}
+
+		});
+
+	}
+
+}
+
+$("input[type=radio]").click(function() {
+
+	$(".custom-control-input, custom-control-label").removeClass("is-invalid");
+
+});
+
+$("#password").on("input", function() {
+
+	if ($("#password").val().length >= 6) {
+
+		$("#password").removeClass("is-invalid");
+
+	}
+
+});
