01cb198797b194b294495731841827473a467e16
bungularbike <rongjackmiles@gmail.com>
2018-10-28 16:03:18 +0800

    Teams with sort/star

--- teams.js
+++ teams.js
@@ -11,10 +11,26 @@ var config = {
 firebase.initializeApp(config);
 var db = firebase.firestore();
 
+var current = "";
 firebase.auth().onAuthStateChanged(function(account) {
 
 	if (account) {
 
+		$(".navbar-brand").html("936" + account.email.substring(0, 1).toUpperCase());
+		current = "936" + account.email.substring(0, 1);
+		if (account.email == "a@hkis.edu.hk") {
+
+			$("#navbar").attr("style", "background-color: #99aaff !important");
+
+		} else if (account.email == "b@hkis.edu.hk") {
+
+			$("#navbar").attr("style", "background-color: #ff9999 !important");
+
+		} else if (account.email == "c@hkis.edu.hk") {
+
+			$("#navbar").attr("style", "background-color: #bababa !important");
+
+		}
 		display();
 
 	} else {
@@ -24,7 +40,7 @@ firebase.auth().onAuthStateChanged(function(account) {
 	}
 
 });
-$("nav button").click(function() {
+$("#signout").click(function() {
 
 	$("nav button").attr("disabled", "true");
 	firebase.auth().signOut().then(function() {
@@ -39,17 +55,123 @@ $("nav button").click(function() {
 
 });
 
+function toggleStar(team) {
+
+	if ($("#team_" + team).hasClass("table-warning")) {
+
+		$("#team_" + team).removeClass("table-warning");
+		db.collection(current).doc(team).update({ star: false }).catch(function(error) {
+
+			alert("An error occurred: " + error.message);
+
+		});
+
+	} else {
+
+		$("#team_" + team).addClass("table-warning");
+		db.collection(current).doc(team).update({ star: true }).catch(function(error) {
+
+			alert("An error occurred: " + error.message);
+
+		});
+
+	}
+
+}
+
 function display() {
 
-	db.collection("936a").get().then(function(qS) {
+	$("tbody").empty();
+	if (filter == "rank" && desc) {
+
+		db.collection("teams").orderBy(filter, (desc ? "desc" : "asc")).get().then(function(qS) {
+
+			qS.forEach(function(doc) {
+
+				var team = doc.data();
+				if (team.rank != Infinity) {
+
+					$("tbody").append("<tr id = 'team_" + team.number + "'><th scope = 'row'>" + team.number + "<br><small>" + team.name + "</small></th><td>" + team.org + "</td><td>" + team.location + "</td><td>" + (team.rank != Infinity ? team.rank : "") + "</td><td>" + team.wp + "</td><td>" + team.ap + "</td><td>" + team.sp + "</td></tr>");
+
+				}
+
+			});
+
+		});
+		db.collection("teams").orderBy(filter, "asc").get().then(function(qS) {
+
+			qS.forEach(function(doc) {
+
+				var team = doc.data();
+				if (team.rank == Infinity) {
+
+					$("tbody").append("<tr id = 'team_" + team.number + "'><th scope = 'row'>" + team.number + "<br><small>" + team.name + "</small></th><td>" + team.org + "</td><td>" + team.location + "</td><td>" + (team.rank != Infinity ? team.rank : "") + "</td><td>" + team.wp + "</td><td>" + team.ap + "</td><td>" + team.sp + "</td></tr>");
+
+				}
+
+			});
+
+		});
+
+	} else {
+
+		db.collection("teams").orderBy(filter, (desc ? "desc" : "asc")).get().then(function(qS) {
+
+			qS.forEach(function(doc) {
+
+				var team = doc.data();
+				$("tbody").append("<tr id = 'team_" + team.number + "'><th scope = 'row'>" + team.number + "<br><small>" + team.name + "</small></th><td>" + team.org + "</td><td>" + team.location + "</td><td>" + (team.rank != Infinity ? team.rank : "") + "</td><td>" + team.wp + "</td><td>" + team.ap + "</td><td>" + team.sp + "</td></tr>");
+
+			});
+
+		});
+
+	}
+	db.collection(current).get().then(function(qS) {
 
 		qS.forEach(function(doc) {
 
-			var team = doc.data();
-			$("tbody").append("<tr><th scope = 'row'>" + team.number + "<br><small>" + team.name + "</small></th><td>" + team.org + "</td><td>" + team.location + "</td></tr>");
+			if (doc.data().star) {
+
+				$("#team_" + doc.id).addClass("table-warning");
+				$("#team_" + doc.id).click(function() {
+
+					toggleStar(doc.id);
+
+				});
+
+			} else {
+
+				$("#team_" + doc.id).click(function() {
+
+					toggleStar(doc.id);
+
+				});
+
+			}
 
 		});
 
 	});
+
+}
+
+var filter = "rank";
+var desc = false;
+function sort(query) {
+
+	if (filter == query) {
+
+		desc = !desc;
+
+	} else {
+
+		filter = query;
+		desc = false;
+
+	}
+	$(".underline").removeClass("underline");
+	$("#heading_" + query).addClass("underline");
+	display();
 
 }
